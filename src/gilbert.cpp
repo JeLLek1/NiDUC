f323eb0ca94aafe47f3d2278bf960ac673b6e08f
Szymon <szymon.pl.com@gmail.com>
2020-04-22 17:17:59 +0200

    gilbert

---
+++ src/gilbert.cpp
@@ -0,0 +1,29 @@
+#include "gilbert.h"
+
+void Gilbert::noiseG(std::vector<uint8_t> &data,int prop1,int prop2)
+{
+	std::srand(time(nullptr));
+	for(unsigned int i=0; i<data.size();i++)
+	{
+		bool isB=false;
+		uint8_t bit=1;
+		for(int j=0;j<8;j++)
+		{	
+			
+			if(isB==false&&(rand()%1000)<prop1)
+			{
+				data[i] ^= bit;
+				isB=true;
+			}
+			else if((rand()%1000)<prop2)
+			{
+				data[i] ^= bit;
+			}
+			else
+			{
+				isB=false;
+			}
+			bit<<=1;
+		}
+	}
+}
---
+++ src/gilbert.h
@@ -0,0 +1,7 @@
+#include <vector>
+
+class Gilbert
+{
+public:
+	static void noiseG(std::vector<uint8_t> &data,int prop1,int prop2)
+};
